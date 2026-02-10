e47d401e3bdf7159ff7e534eb2bc8c7c5378dc39
hafizullohkomilov013-cyber <hafizullohkomilov013@gmail.com>
2026-02-10 21:55:55 +0500

    the anf

--- src/components/SkilsCard.jsx
+++ src/components/SkilsCard.jsx
@@ -4,21 +4,6 @@ import { useTranslation } from 'react-i18next';
 console.log(skillsData);
 
 
-function SkilsCard() {
-    let [t] =useTranslation()
-  return (
-    <section className="container">
-      <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-        {skillsData.map((item) => (
-          <div
-            key={item.id}
-            className="w-full min-w-50 bg-white rounded-2xl p-6 shadow-sm"
-          >
-            <img src={t(item.icon)} alt="" />
-            <h2>{t(item.titleKey)}</h2>
-            <p>{t(item.descKey)}</p>
-          </div>
-        ))}
       </div>
     </section>
   );
