91ca8e103b2b2a90d1c665c4d8252fbe04c68d3c
hafizullohkomilov013-cyber <hafizullohkomilov013@gmail.com>
2026-02-11 18:22:02 +0500

    the and

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,14 @@
 import Link from "../assets/img/Link.svg"
+import { useTranslation } from "react-i18next";
 
 function Footer() {
+    const [t] = useTranslation()
   return (
     <section className="bg-[#F8F9FA] py-12">
       <div className="container">
         <div>
-            <img src={Link} alt="" />
-            <p></p>
+          <img src={Link} alt="" />
+          <p>{t("footer.copyright")}</p>
         </div>
       </div>
     </section>
